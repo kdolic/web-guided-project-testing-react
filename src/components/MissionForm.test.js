cc95c37e7aede73c036c819adc56545878ed8900
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-25 13:06:58 -0500

    lecture work-in-progress

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -30,7 +30,7 @@ test('calls getData when the button is pressed', ()=>{
     const fakeGetData = jest.fn();
 
     //Arrange
-    render(<MissionForm isFetchingData={false} getData={}/>);
+    render(<MissionForm isFetchingData={false}/>);
 
     //Act:
     const button = screen.getByRole('button');
