2cf67e90ccf9e8fd0cce82c4b3f9233d587ef1ec
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-25 13:29:27 -0500

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -3,3 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App';
 
+test('Renders without errors', ()=> {
+    render(<App/>);
+});
+
