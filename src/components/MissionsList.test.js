069cd369a5ec624fe93a8e76af845df65dfb44aa
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-25 13:55:54 -0500

    lecture work-in-progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -2,6 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import MissionsList from "./MissionsList";
 
+
+
+const missions = [
+    {
+        mission_name: "Thaicom",
+        mission_id: "9D1B7E0"
+    },
+    {
+        mission_name: "Telstar",
+        mission_id: "F4F83DE"
+    }
+];
+
 test('renders without errors', ()=>{
-    render(<MissionsList/>);
+    render(<MissionsList missions={missions}/>);
 });
