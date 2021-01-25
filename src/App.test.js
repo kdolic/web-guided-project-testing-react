9273fd16c6323099834aa13992d9de67d4176d8b
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-25 13:31:04 -0500

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -8,5 +8,7 @@ test('Renders without errors', ()=> {
 });
 
 test('Renders mission data when button is clicked', ()=> {
-    
+    //Arrange: render app
+    //Act: get data from api? and push button
+    //Assert: We should get the same amount of missions as our api returned.
 });
