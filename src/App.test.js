a814bf7a5394cb735bc12025bcac4850052aa39f
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-25 13:45:08 -0500

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App';
 
+import mockFetchMissions from './api/fetchMissions';
+jest.mock('./api/fetchMissions');
+
 test('Renders without errors', ()=> {
     render(<App/>);
 });
